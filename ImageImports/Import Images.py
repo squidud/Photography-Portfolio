5ed1d65839391d8497152b9b819abd7af5047d00
Matthew Reichard <squid05@hotmail.com>
2025-07-16 18:53:13 -0500

    fixed virtual environment getting uploaded (which halted the build)

--- ImageImports/Import Images.py
+++ ImageImports/Import Images.py
@@ -3,17 +3,25 @@ import platform
 import subprocess
 import sys
 
-def main():
-    # activate virtualenv manually (only needed if dependencies must be loaded *in* this script)
+def getVenv():
     if platform.system() == "Windows":
-        activate_script = os.path.join("venv", "Scripts", "activate.bat")
         python_exe = os.path.join("venv", "Scripts", "python.exe")
     else:
-        activate_script = "source venv/bin/activate"
         python_exe = os.path.join("venv", "bin", "python")
+
+
+def main():
+    # activate virtualenv manually (only needed if dependencies must be loaded *in* this script)
+    if os.path.isdir('venv'):
+        python_exe = getVenv()
+    else:
+        subprocess.run([sys.executable, "-m", "venv", "venv"], check=True)
+        python_exe = getVenv()
+        subprocess.run([python_exe, "-m", "pip", "install", "-r", "requirements.txt"], check=True)    
 
     # Run the actual processing script
     subprocess.run([python_exe, "ImageImports/converterscript.py"])
 
 if __name__ == "__main__":
     main()
+
